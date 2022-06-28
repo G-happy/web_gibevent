3507d49a8423b11f667b97caac9d26559ccd200b
GHL <1569215575@qq.com>
2022-06-28 14:45:16 +0800

    ....

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -11,8 +11,8 @@ $(function() {
             success: (res) => {
                 const htmlStr = template("tpl-table", res);
                 $("tbody").html(htmlStr);
-            }
-        })
+            },
+        });
     }
 
     // 监听 "添加类别" 按钮点击事件
@@ -22,10 +22,10 @@ $(function() {
         indexAdd = layer.open({
             type: 1,
             area: ["500px", "250px"],
-            title: '添加文章分类',
+            title: "添加文章分类",
             content: $("#dialog-add").html(),
         });
-    })
+    });
 
     // 通过代理的形式,为 form-add 表单绑定提交事件
     $("body").on("submit", "#form-add", function(e) {
@@ -36,14 +36,16 @@ $(function() {
             data: $(this).serialize(),
             success: (res) => {
                 if (res.status !== 0) {
-                    return layer.msg("新增文章分类失败！")
+                    return layer.msg("新增文章分类失败！");
                 }
+                // 初始化界面
                 initArticleList();
                 layer.msg("新增文章分类成功！");
+                // 关闭弹窗
                 layer.close(indexAdd);
-            }
-        })
-    })
+            },
+        });
+    });
 
     // 通过代理的形式,为 edit-btn 按钮绑定点击事件
     let indexEdit = null;
@@ -52,7 +54,7 @@ $(function() {
         indexEdit = layer.open({
             type: 1,
             area: ["500px", "250px"],
-            title: '修改文章分类',
+            title: "修改文章分类",
             content: $("#dialog-edit").html(),
         });
         let id = $(this).attr("data-id");
@@ -60,13 +62,13 @@ $(function() {
         // 获取点击项的 Id 值
         $.ajax({
             method: "GET",
-            url: '/my/article/cates/' + id,
+            url: "/my/article/cates/" + id,
             success: (res) => {
                 // 为表单填充原有的值
                 form.val("form-edit", res.data);
-            }
-        })
-    })
+            },
+        });
+    });
 
     // 通过代理的形式,为 form-edit 表单绑定submit事件
     $("body").on("submit", "#form-edit", function(e) {
@@ -77,33 +79,32 @@ $(function() {
             data: $(this).serialize(),
             success: (res) => {
                 if (res.status !== 0) {
-                    return layer.msg("更新分类信息失败！")
+                    return layer.msg("更新分类信息失败！");
                 }
                 layer.msg("更新分类信息成功！");
                 layer.close(indexEdit);
                 initArticleList();
-            }
-        })
-    })
+            },
+        });
+    });
 
     // 通过代理的形式,为 delete-btn 按钮绑定删除事件
     $("body").on("click", ".delete-btn", function() {
         const id = $(this).attr("data-id");
         // 提示删除框
-        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+        layer.confirm("确认删除?", { icon: 3, title: "提示" }, function(index) {
             $.ajax({
                 method: "GET",
                 url: "/my/article/deletecate/" + id,
-                success: res => {
+                success: (res) => {
                     if (res.status !== 0) {
                         return layer.msg("删除文章分类失败！");
                     }
                     layer.msg("删除文章分类成功！");
                     layer.close(index);
                     initArticleList();
-
-                }
-            })
+                },
+            });
         });
-    })
-})
+    });
+});
--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -3,31 +3,33 @@ $(function() {
     let form = layui.form;
     // 美化时间的过滤器
     template.defaults.imports.dataFormat = function(data) {
-            const dt = new Date(data);
+        const dt = new Date(data);
 
-            let year = dt.getFullYear();
-            let month = dt.getMonth() + 1;
-            let days = dt.getDate();
+        let year = dt.getFullYear();
+        let month = dt.getMonth() + 1;
+        let days = dt.getDate();
 
-            let hour = dt.getHours();
-            let minutes = dt.getMinutes();
-            let second = dt.getSeconds();
+        let hour = dt.getHours();
+        let minutes = dt.getMinutes();
+        let second = dt.getSeconds();
 
-            // 补 0
-            month = month >= 10 ? month : "0" + month;
-            days = days >= 10 ? days : "0" + days;
-            hour = hour >= 10 ? hour : "0" + hour;
-            minutes = minutes >= 10 ? minutes : "0" + minutes;
-            second = second >= 10 ? second : "0" + second;
-            return year + "-" + month + "-" + days + " " + hour + ":" + minutes + ":" + second;
-        }
-        // 定义一个查询对象，将来请求参数的时候，需要将请求参数对象提交到服务器
+        // 补 0
+        month = month >= 10 ? month : "0" + month;
+        days = days >= 10 ? days : "0" + days;
+        hour = hour >= 10 ? hour : "0" + hour;
+        minutes = minutes >= 10 ? minutes : "0" + minutes;
+        second = second >= 10 ? second : "0" + second;
+        return (
+            year + "-" + month + "-" + days + " " + hour + ":" + minutes + ":" + second
+        );
+    };
+    // 定义一个查询对象，将来请求参数的时候，需要将请求参数对象提交到服务器
     const q = {
         pagenum: 1, // 页码值
-        pagesize: 2, // 每页显示多少条数据,默认为 2 
+        pagesize: 2, // 每页显示多少条数据,默认为 2
         cate_id: "", //	文章分类的 Id
         state: "", // 文章的状态，可选值有：已发布、草稿
-    }
+    };
     initTable();
     initCate();
     // 获取文章列表数据，并渲染到界面上
@@ -38,16 +40,16 @@ $(function() {
             data: q,
             success: (res) => {
                 if (res.status !== 0) {
-                    return layer.msg("获取文章列表失败！")
+                    return layer.msg("获取文章列表失败！");
                 }
                 // 使用模板引擎渲染界面
                 let htmlStr = template("tpl-table", res);
-                $("tbody").html(htmlStr)
+                $("tbody").html(htmlStr);
 
                 // 调用渲染分页的函数
                 renderPage(res.total);
-            }
-        })
+            },
+        });
     }
 
     // 初始化文章分类的方法
@@ -57,7 +59,7 @@ $(function() {
             url: "/my/article/cates",
             success: function(res) {
                 if (res.status !== 0) {
-                    return layer.msg("获取文章分类列表失败！")
+                    return layer.msg("获取文章分类列表失败！");
                 }
 
                 // 调用模板引擎渲染分类的可选项
@@ -65,8 +67,8 @@ $(function() {
                 $("[name=cate_id]").html(htmlStr);
                 // 通知 layui 重新渲染表单结构UI区域
                 form.render();
-            }
-        })
+            },
+        });
     }
 
     // 为表单绑定 submit 事件
@@ -83,27 +85,27 @@ $(function() {
 
         // 根据最新的查询条件，查询渲染界面
         initTable();
-    })
+    });
 
     // 分页
     function renderPage(total) {
         var laypage = layui.laypage;
         laypage.render({
-            elem: 'pageBox', // 注意，这里的 test1 是 ID，不用加 # 号
+            elem: "pageBox", // 注意，这里的 test1 是 ID，不用加 # 号
             count: total, // 数据总数，从服务端得到
             limit: q.pagesize, // 每页显示的条数。
             curr: q.pagenum, // 起始页
-            layout: ["count", "limit", "prev", 'page', 'next', "skip"],
+            layout: ["count", "limit", "prev", "page", "next", "skip"],
             limits: [2, 3, 5, 10],
-            // 分页发生切换时执行的回调函数 
+            // 分页发生切换时执行的回调函数
             /**
              * 触发 jump 回调的两种方式：
              * 1.点击页码的时候，触发 jump 回调
              * 2.只要调用了 laypage.render 方法，就会触发 jump 回调
-             * 
+             *
              * 第一种方式触发 jump 回调，first 的值为 undefined
              * 第二种方式触发 jump 回调，first 的值为 true
-             *  
+             *
              */
             jump: function(obj, first) {
                 // console.log(obj.curr);
@@ -115,26 +117,24 @@ $(function() {
                 if (!first) {
                     initTable();
                 }
-            }
+            },
         });
     }
 
     // 通过代理的形式，为删除按钮绑定点击事件
     $("body").on("click", ".delete-btn", function() {
-
         // 获取删除按钮的个数
         let delBtnNum = $(".delete-btn").length;
         let id = $(this).attr("data-id");
 
         // 删除弹窗
-        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
-
+        layer.confirm("确认删除?", { icon: 3, title: "提示" }, function(index) {
             $.ajax({
                 method: "GET",
                 url: "/my/article/delete/" + id,
                 success: (res) => {
                     if (res.status !== 0) {
-                        return layer.msg("删除失败！")
+                        return layer.msg("删除失败！");
                     }
                     // 当前页已经没有数据
                     if (delBtnNum === 1) {
@@ -142,10 +142,9 @@ $(function() {
                         q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1;
                     }
                     initTable();
-
-                }
-            })
+                },
+            });
             layer.close(index);
         });
-    })
-})
+    });
+});
